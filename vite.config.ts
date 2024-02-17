bb1d049202cba35ede31a30631627ea5b210b5e4
Nate Emerson <emerson.nate@gmail.com>
2024-02-17 13:49:36 -0800

    First try at D1 bindings

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import pages from '@hono/vite-cloudflare-pages'
 import honox from 'honox/vite'
 import client from 'honox/vite/client'
 import { defineConfig } from 'vite'
+import { getEnv } from '@hono/vite-dev-server/cloudflare-pages'
+import devServer from '@hono/vite-dev-server'
 
 export default defineConfig(({ mode }) => {
   if (mode === 'client') {
@@ -10,7 +12,17 @@ export default defineConfig(({ mode }) => {
     }
   } else {
     return {
-      plugins: [honox(), pages()]
+      plugins: [
+        honox(),
+        pages(),
+        devServer({
+          entry: 'app/server.ts',
+          env: getEnv({
+            d1Databases: 'DB',
+            d1Persist: true,
+          })
+        })
+      ]
     }
   }
 })
