09b452e85797f76983c09a95809b6741cfcbac9a
Nate Emerson <emerson.nate@gmail.com>
2024-02-17 11:49:30 -0800

    Update client entry point

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -1,6 +1,6 @@
 import { Style } from 'hono/css'
 import { jsxRenderer } from 'hono/jsx-renderer'
-import { Script } from 'honox/server'
+import { Script, HasIslands } from 'honox/server'
 
 export default jsxRenderer(({ children, title }) => {
   return (
@@ -9,6 +9,13 @@ export default jsxRenderer(({ children, title }) => {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{title}</title>
+        { import.meta.env.PROD ? (
+          <HasIslands>
+            <script type="module" src="/static/client.ts"></script>
+          </HasIslands>
+        ) : (
+          <script type="module" src="/app/client.ts"></script>
+        )}
         <Script src="/app/client.ts" />
         <Style />
       </head>
