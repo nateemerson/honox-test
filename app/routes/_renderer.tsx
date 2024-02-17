730b1b7ae15fe057296b34e0529bcab7fd3feb85
Nate Emerson <emerson.nate@gmail.com>
2024-02-17 12:29:18 -0800

    Test prod and remove script

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -9,14 +9,13 @@ export default jsxRenderer(({ children, title }) => {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{title}</title>
-        { import.meta.env.PROD ? (
+        { true ? (
           <HasIslands>
             <script type="module" src="/static/client.ts"></script>
           </HasIslands>
         ) : (
           <script type="module" src="/app/client.ts"></script>
         )}
-        <Script src="/app/client.ts" />
         <Style />
       </head>
       <body>{children}</body>
