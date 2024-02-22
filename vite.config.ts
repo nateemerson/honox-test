7d10f6eb8403ef0ff13b7f4567a8eb721a71f518
Nate Emerson <emerson.nate@gmail.com>
2024-02-22 07:18:44 -0800

    Fix vite dev server

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,24 @@ import pagesBuild from '@hono/vite-cloudflare-pages'
 import honox from 'honox/vite'
 import clientBuild from 'honox/vite/client'
 import { defineConfig } from 'vite'
-import pagesPlugin from '@hono/vite-dev-server/cloudflare-pages'
+import { getPlatformProxy } from 'wrangler'
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(async ({ mode }) => {
   if (mode === 'client') {
     return {
       plugins: [clientBuild()]
     }
   } else {
+    const { env, dispose } = await getPlatformProxy()
     return {
       plugins: [
         honox({
           devServer: {
+            env,
             plugins: [
-              pagesPlugin({
-                d1Databases: ['MY_D1_DB'],
-                d1Persist: true,
-              })
+              {
+                onServerClose: dispose
+              }
             ]
           }
         }),
